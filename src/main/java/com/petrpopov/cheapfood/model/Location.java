bb60efe097ba5f44d85f667856e999290cbc675b
Petr Popov <popov.petr@gmail.com>
2013-07-02 17:29:57 +0400

    first commit

---
+++ src/main/java/com/petrpopov/cheapfood/model/Location.java
@@ -0,0 +1,9 @@
+package com.petrpopov.cheapfood.model;
+
+/**
+ * User: petrpopov
+ * Date: 02.07.13
+ * Time: 17:01
+ */
+public class Location {
+}
---
+++ src/main/java/com/petrpopov/cheapfood/web/HomeController.java
@@ -0,0 +1,23 @@
+package com.petrpopov.cheapfood.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
+
+/**
+ * User: petrpopov
+ * Date: 02.07.13
+ * Time: 17:18
+ */
+@Controller
+public class HomeController {
+
+    @RequestMapping({"/","/home", "/index", "/main"})
+    public String showHomePage(@CookieValue(required = false, value = "guid") String cookie,
+                               Map<String, Object> model)
+    {
+        return "index";
+    }
+}
